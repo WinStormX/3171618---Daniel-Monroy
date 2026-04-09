fd1e81393c92a29415243de77bad16c5986b6653
WinStormX <danielmonroymc@gmail.com>
2026-04-09 08:31:08 -0500

    Main.tsx

--- Week2/src/main.tsx
+++ Week2/src/main.tsx
@@ -3,8 +3,3 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/App.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
